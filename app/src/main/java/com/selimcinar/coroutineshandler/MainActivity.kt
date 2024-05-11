e06fbe8b96e127ab28751b418bdd826d5d9b0755
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2024-05-11 18:28:27 +0300

    Update MainActivity.kt

--- app/src/main/java/com/selimcinar/coroutineshandler/MainActivity.kt
+++ app/src/main/java/com/selimcinar/coroutineshandler/MainActivity.kt
@@ -5,6 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
